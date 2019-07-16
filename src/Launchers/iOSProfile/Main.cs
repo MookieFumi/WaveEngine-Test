05faf4300040400b057222e9ec682896628f7cb5
Miguel Angel Martin Hernandez <miguel.martin@neoris.com>
2019-07-16 10:26:49 +0200

    Add iOS profile

---
+++ src/Launchers/iOSProfile/Main.cs
@@ -0,0 +1,12 @@
+using UIKit;
+
+namespace XamarinForms3DCarSample
+{
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
